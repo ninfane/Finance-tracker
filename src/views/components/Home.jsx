94cf0325f5569401a3cfbfa7d2a761bad7f31ebf
ninfane <marinacrespog@gmail.com>
2022-02-06 16:13:51 -0300

    Home UI ready

--- src/views/components/Home.jsx
+++ src/views/components/Home.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import Barrita from './Barrita';
+import React, {useState} from 'react'
+import Barrita from './Barrita'
 import style from './css/Home.module.css'
 
 const Home = () => {
-  return <div>
-    {/* amount va a venir del state */}
-    <Barrita title={'Ingresos'} amount={'$100'}/>
-    <Barrita title={'Gastos'} amount={'$90'}/>
-    <Barrita title={'Resto'} amount={'$10'}/>
-  </div>
-};
+  const [ingresos, setIngresos] = useState(0);
+  const [gastos, setGastos] = useState(0);
+  const [resto, setResto] = useState(ingresos-gastos);
 
-export default Home;
+  return (
+      <div>
+          <div className={style.container}>
+              {/* amount va a venir del state */}
+              <Barrita title={'Ingresos'} amount={ingresos} />
+              <Barrita title={'Gastos'} amount={gastos} />
+              <Barrita title={'Resto'} amount={resto} />
+          </div>
+          <form className={style.form}>
+            <input className={style.input} type="text" placeholder='Ingreso total'/>
+            <button type='submit' className={style.pinkBtn}>Ingresar</button>
+          </form>
+          <form className={style.form}>
+            <input className={style.input} type="text" placeholder='concepto'/>
+            <input className={style.input} type="number" placeholder='monto con decimales'/>
+            <button type='submit' className={style.pinkBtn}>Ingresar</button>
+          </form>
+      </div>
+  )
+}
+
+export default Home
