dc03f7467568f157305c7d04cade72d55ef20363
ninfane <marinacrespog@gmail.com>
2022-02-20 00:04:33 -0300

    finished basic use

--- src/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-
-//aca se agregan todos los reducers
-const allReducers = combineReducers({})
-
-export default (state, action) => {
-    return allReducers(state, action)
-}
--- src/views/components/Home.jsx
+++ src/views/components/Home.jsx
@@ -1,11 +1,29 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Barrita from './Barrita'
 import style from './css/Home.module.css'
 
 const Home = () => {
   const [ingresos, setIngresos] = useState(0);
-  const [gastos, setGastos] = useState(0);
-  const [resto, setResto] = useState(ingresos-gastos);
+  const [inputIngreso, setInputIngreso] = useState('');
+  const [inputGasto, setInputGasto] = useState('');
+  const [gastos, setGastos] = useState(100);
+  const [resto, setResto] = useState('');
+
+  useEffect(() => {
+    setResto(parseInt(ingresos) - parseInt(gastos));
+  })
+  
+  const submitIngreso = (e) => {
+    e.preventDefault();
+    setIngresos(inputIngreso);
+    setInputIngreso('');
+  }
+
+  const submitGasto = (e) => {
+    e.preventDefault();
+    setGastos(parseInt(gastos)+ parseInt(inputGasto));
+    setInputGasto('');
+  }
 
   return (
       <div>
@@ -16,12 +34,12 @@ const Home = () => {
               <Barrita title={'Resto'} amount={resto} />
           </div>
           <form className={style.form}>
-            <input className={style.input} type="text" placeholder='Ingreso'/>
-            <button type='submit' className={style.pinkBtn}>Ingresar</button>
+            <input className={style.input} type="number" placeholder='Ingreso' value={inputIngreso} onChange={(e) => setInputIngreso(e.target.value)}></input>
+            <button type='submit' className={style.pinkBtn} onClick={(e) => submitIngreso(e)}>Ingresar</button>
           </form>
           <form className={style.form}>
-            <input className={style.input} type="number" placeholder='Gasto'/>
-            <button type='submit' className={style.pinkBtn}>Ingresar</button>
+            <input className={style.input} type="number" placeholder='Gasto' value={inputGasto} onChange={(e) => setInputGasto(e.target.value)}/>
+            <button type='submit' className={style.pinkBtn} onClick={(e) => submitGasto(e)}>Ingresar</button>
           </form>
       </div>
   )
